9086ca6c2fa4d5cf810a24436909a931d5506913
Mateo Iglesias <contact.prodzodi@gmail.com>
2023-12-11 12:54:50 -0300

    router.query

--- pages/seguimiento/[id].jsx
+++ pages/seguimiento/[id].jsx
@@ -7,79 +7,79 @@ import "tailwindcss/tailwind.css";
 
 function DetalleSeguimiento() {
   const router = useRouter();
-  // const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
 
-  // useEffect(() => {
-  //   if (router.query.id) {
-  //     axios
-  //       .post(`/api/correo/`, { id: router.query.id })
-  //       .then((res) => {
-  //         setData(res.data);
-  //         data.pop();
-  //       })
-  //       .catch((err) => {
-  //         // Manejar el error
-  //         console.error(err);
-  //       });
-  //   }
-  // }, [router.query.id]);
+  useEffect(() => {
+    if (router.query.id) {
+      axios
+        .post(`/api/correo/`, { id: router.query.id })
+        .then((res) => {
+          setData(res.data);
+          data.pop();
+        })
+        .catch((err) => {
+          // Manejar el error
+          console.error(err);
+        });
+    }
+  }, [router.query.id]);
 
-  const data = [
-    {
-      Fecha: "01-12-2023 11:11",
-      Planta: "CENTRO PAQUETERIA QUILMES",
-      Historia: "INTENTO DE ENTREGA",
-      Estado: "ENTREGADO",
-    },
-    {
-      Fecha: "01-12-2023 07:51",
-      Planta: "CENTRO PAQUETERIA QUILMES",
-      Historia: "EN PODER DEL DISTRIBUIDOR",
-      Estado: "",
-    },
-    {
-      Fecha: "01-12-2023 03:54",
-      Planta: "CENTRO PAQUETERIA QUILMES",
-      Historia: "LLEGADA AL CENTRO DE PROCESAMIENTO",
-      Estado: "",
-    },
-    {
-      Fecha: "30-11-2023 22:49",
-      Planta: "PAQUETERIA VICENTE LOPEZ",
-      Historia: "EN PROCESO DE CLASIFICACIÓN",
-      Estado: "",
-    },
-    {
-      Fecha: "30-11-2023 19:35",
-      Planta: "PAQUETERIA VICENTE LOPEZ",
-      Historia: "LLEGADA AL CENTRO DE PROCESAMIENTO",
-      Estado: "",
-    },
-    {
-      Fecha: "30-11-2023 17:17",
-      Planta: "MAXIKIOSCO",
-      Historia: "EN PROCESO DE CLASIFICACIÓN",
-      Estado: "",
-    },
-    {
-      Fecha: "30-11-2023 14:48",
-      Planta: "MAXIKIOSCO",
-      Historia: "INGRESO AL CORREO",
-      Estado: "",
-    },
-    {
-      Fecha: "30-11-2023 14:48",
-      Planta: "MAXIKIOSCO",
-      Historia: "REPESAJE",
-      Estado: "",
-    },
-    {
-      Fecha: "30-11-2023 10:48",
-      Planta: "CORREO ARGENTINO",
-      Historia: "PREIMPOSICION",
-      Estado: "",
-    },
-  ];
+  // const data = [
+  //   {
+  //     Fecha: "01-12-2023 11:11",
+  //     Planta: "CENTRO PAQUETERIA QUILMES",
+  //     Historia: "INTENTO DE ENTREGA",
+  //     Estado: "ENTREGADO",
+  //   },
+  //   {
+  //     Fecha: "01-12-2023 07:51",
+  //     Planta: "CENTRO PAQUETERIA QUILMES",
+  //     Historia: "EN PODER DEL DISTRIBUIDOR",
+  //     Estado: "",
+  //   },
+  //   {
+  //     Fecha: "01-12-2023 03:54",
+  //     Planta: "CENTRO PAQUETERIA QUILMES",
+  //     Historia: "LLEGADA AL CENTRO DE PROCESAMIENTO",
+  //     Estado: "",
+  //   },
+  //   {
+  //     Fecha: "30-11-2023 22:49",
+  //     Planta: "PAQUETERIA VICENTE LOPEZ",
+  //     Historia: "EN PROCESO DE CLASIFICACIÓN",
+  //     Estado: "",
+  //   },
+  //   {
+  //     Fecha: "30-11-2023 19:35",
+  //     Planta: "PAQUETERIA VICENTE LOPEZ",
+  //     Historia: "LLEGADA AL CENTRO DE PROCESAMIENTO",
+  //     Estado: "",
+  //   },
+  //   {
+  //     Fecha: "30-11-2023 17:17",
+  //     Planta: "MAXIKIOSCO",
+  //     Historia: "EN PROCESO DE CLASIFICACIÓN",
+  //     Estado: "",
+  //   },
+  //   {
+  //     Fecha: "30-11-2023 14:48",
+  //     Planta: "MAXIKIOSCO",
+  //     Historia: "INGRESO AL CORREO",
+  //     Estado: "",
+  //   },
+  //   {
+  //     Fecha: "30-11-2023 14:48",
+  //     Planta: "MAXIKIOSCO",
+  //     Historia: "REPESAJE",
+  //     Estado: "",
+  //   },
+  //   {
+  //     Fecha: "30-11-2023 10:48",
+  //     Planta: "CORREO ARGENTINO",
+  //     Historia: "PREIMPOSICION",
+  //     Estado: "",
+  //   },
+  // ];
 
   return (
     <>
--- pages/utils/index.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios");
-
-export default async function getSeguimiento(id) {
-  const seguimiento = await axios.post("/api/correo", {
-    id: id,
-  });
-
-  return seguimiento;
-}
